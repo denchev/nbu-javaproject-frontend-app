78bb87ddc0d8ca210b92d9a7a6e25de320bd1c28
Marush Denchev <avreon@gmail.com>
2021-08-12 07:53:19 +0300

    Doctor login ui

--- src/components/DoctorLogin.js
+++ src/components/DoctorLogin.js
@@ -1,11 +1,24 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class DoctorLogin extends Component {
     render() {
         return (
-            <div>
-                Login as doctor
-            </div>
+            <React.Fragment>
+                <h4>Login as doctor</h4>
+                <div className="form">
+                    <div className="mb-3">
+                        <label for="doctorUin" className="form-label">UIN</label>
+                        <input type="text" className="form-control" id="doctorUin" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="doctorPassword" className="form-label">Password</label>
+                        <input type="text" className="form-control" id="doctorPassword" />
+                    </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
